21fff397e21ff41fad0289fabc0c57c71b87e594
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-06-07 04:03:32 +0900

    Change bundler to rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "./src/index.js",
+  output: [
+    {
+      file: "dist/atv.js",
+      format: "cjs",
+      sourcemap: false,
+    },
+    {
+      file: "dist/atv.min.js",
+      format: "cjs",
+      sourcemap: false,
+      compact: true,
+    },
+  ],
+  plugins: [
+    resolve({ jsnext: true }),
+    commonjs(),
+  ],
+};
